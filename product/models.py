02a3cf888588b79d66a754352d3909e6e238d223
alfin <alfin@linkaja.id>
2022-09-11 14:02:21 +0700

    create models service product

---
+++ product/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class CreateUpdate(models.Model):
+    created_date = models.DateTimeField(blank=True, null=True)
+    update_date = models.DateTimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.created_date:
+            self.created_date = timezone.now()
+        self.update_date = timezone.now()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+        
+class Product(CreateUpdate):
+    class ProductStatus(models.TextChoices):
+        PUBLISH = 'PUBLISH', "PUBLISH"
+        PENDING = 'PENDING', "PENDING"
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    status = models.CharField(
+        max_length=15,
+        choices=ProductStatus.choices,
+        default=ProductStatus.PENDING
+    )
+    
+    class Meta:
+        db_table = 'product'
